564ebf5c2bc5e0d492267b3b6fc6bcc253e8e25a
Junjie <yjj950406@gmail.com>
2021-09-27 15:20:54 -0400

    setup notification for IOS

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Junjie Yang on 9/27/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,13 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    if #available(iOS 10.0, *) {
+           application.applicationIconBadgeNumber = 0 // For Clear Badge Counts
+           let center = UNUserNotificationCenter.current()
+           center.removeAllDeliveredNotifications() // To remove all delivered notifications
+           center.removeAllPendingNotificationRequests()
+       }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
