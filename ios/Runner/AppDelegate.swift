66244bb6e7f25fae0e88cdb262482c41ae295175
Junjie <yjj950406@gmail.com>
2021-09-27 17:33:48 -0400

    bug fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import UserNotifications
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
--- lib/backend/services/ib_cloud_messaging_service.dart
+++ lib/backend/services/ib_cloud_messaging_service.dart
@@ -66,4 +66,24 @@ class IbCloudMessagingService extends GetConnect {
     });
     print(result.data);
   }
+
+  Future<void> sendPushMessage() async {
+    if (_token == null) {
+      print('Unable to send FCM message, no token exists.');
+      return;
+    }
+
+    try {
+      await http.post(
+        Uri.parse('https://api.rnfirebase.io/messaging/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: constructFCMPayload(_token),
+      );
+      print('FCM request for device sent!');
+    } catch (e) {
+      print(e);
+    }
+  }
 }
