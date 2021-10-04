daafbe906ee0154ff0997749b15a54fd24757f54
Junjie <yjj950406@gmail.com>
2021-10-04 16:22:14 -0400

    disable crashlytics in debug mode

--- lib/backend/controllers/main_controller.dart
+++ lib/backend/controllers/main_controller.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -17,8 +18,16 @@ class MainController extends GetxController {
     try {
       await GetStorage.init();
       await Firebase.initializeApp();
+
       //SetUp Crashlytics
-      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+      if (kDebugMode) {
+        await FirebaseCrashlytics.instance
+            .setCrashlyticsCollectionEnabled(false);
+      } else {
+        await FirebaseCrashlytics.instance
+            .setCrashlyticsCollectionEnabled(true);
+      }
+
       FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
       // Pass all uncaught errors to Crashlytics.
       final void Function(FlutterErrorDetails)? originalOnError =
