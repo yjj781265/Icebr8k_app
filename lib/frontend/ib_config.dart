75483b333d644de883bad7c10a9e5ea951409c08
Junjie Yang <yjj950406@gmail.com>
2022-06-21 18:00:41 -0400

    change version code in prod

--- lib/frontend/ib_config.dart
+++ lib/frontend/ib_config.dart
@@ -45,7 +45,7 @@ class IbConfig {
   static const int kCircleMaxMembers = 88;
   static const String kDefaultCoverPhotoUrl =
       'https://firebasestorage.googleapis.com/v0/b/icebr8k-flutter.appspot.com/o/images%2Fheader_img.jpg?alt=media&token=070dea5e-269a-467c-acb8-35ba7a5fde39';
-  static const double kVersion = 0.2;
+  static const double kVersion = 1.0;
 
   IbConfig._();
 }
