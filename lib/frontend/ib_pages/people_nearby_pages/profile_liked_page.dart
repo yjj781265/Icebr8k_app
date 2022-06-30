e346c3d3abccbb1e341836755a2b555af13bc349
Junjie Yang <yjj950406@gmail.com>
2022-06-30 14:03:57 -0400

    bug fix in nearby

--- lib/frontend/ib_pages/people_nearby_pages/profile_liked_page.dart
+++ lib/frontend/ib_pages/people_nearby_pages/profile_liked_page.dart
@@ -18,97 +18,110 @@ class ProfileLikedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('People who liked you'),
-      ),
-      body: Obx(() {
-        if (_controller.likeItems.isEmpty) {
-          return Center(
-              child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Lottie.asset('assets/images/dog.json'),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                    'No one has liked you yet, be patient, they will come!'),
-              )
-            ],
-          ));
-        }
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('People who liked you'),
+        ),
+        body: Obx(() {
+          if (_controller.likeItems.isEmpty) {
+            return Center(
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Lottie.asset('assets/images/dog.json'),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                      'No one has liked you yet, be patient, they will come!'),
+                )
+              ],
+            ));
+          }
 
-        return SmartRefresher(
-          controller: _controller.likeRefreshController,
-          enablePullDown: false,
-          enablePullUp: _controller.likeItems.length >= IbConfig.kPerPage,
-          onLoading: () async {
-            await _controller.loadMoreLikedItems();
-          },
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              final item = _controller.likeItems[index];
-              return IbCard(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    onTap: () {
-                      Get.to(() => ProfilePage(Get.put(
-                          ProfileController(item.user.id),
-                          tag: item.user.id)));
-                    },
-                    leading: IbUserAvatar(
-                      avatarUrl: item.user.avatarUrl,
-                    ),
-                    title: Text(
-                      item.user.username,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: IbConfig.kNormalTextSize),
-                    ),
-                    subtitle: Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      children: [
-                        Text('🔍${item.user.intentions.join(" · ")}'),
-                        if (item.distanceInMeters != null)
-                          Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Text(
+          return SmartRefresher(
+            controller: _controller.likeRefreshController,
+            enablePullDown: false,
+            enablePullUp: _controller.likeItems.length >= IbConfig.kPerPage,
+            onLoading: () async {
+              await _controller.loadMoreLikedItems();
+            },
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                final item = _controller.likeItems[index];
+                return IbCard(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      onTap: () {
+                        Get.to(() => ProfilePage(Get.put(
+                            ProfileController(item.user.id),
+                            tag: item.user.id)));
+                      },
+                      leading: IbUserAvatar(
+                        avatarUrl: item.user.avatarUrl,
+                      ),
+                      title: Text(
+                        '${item.user.username}, ${IbUtils.calculateAge(item.user.birthdateInMs ?? 0).toString()}',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: IbConfig.kNormalTextSize),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Wrap(
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            children: [
+                              Text('🔍 ${item.user.intentions.join(" · ")}'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          if (item.isBingo)
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Icon(
+                                  Icons.favorite,
+                                  color: IbColors.errorRed,
+                                ),
+                                Icon(
+                                  Icons.favorite,
+                                  color: IbColors.errorRed,
+                                )
+                              ],
+                            )
+                          else
+                            const Icon(
+                              Icons.favorite,
+                              color: IbColors.errorRed,
+                            ),
+                          if (item.distanceInMeters != null)
+                            Text(
                               IbUtils.getDistanceString(
                                   item.distanceInMeters!.toDouble()),
                               style: const TextStyle(
                                   fontSize: IbConfig.kDescriptionTextSize),
-                            ),
-                          )
-                      ],
+                            )
+                        ],
+                      ),
                     ),
-                    trailing: item.isBingo
-                        ? Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: const [
-                              Icon(
-                                Icons.favorite,
-                                color: IbColors.errorRed,
-                              ),
-                              Icon(
-                                Icons.favorite,
-                                color: IbColors.errorRed,
-                              )
-                            ],
-                          )
-                        : const Icon(
-                            Icons.favorite,
-                            color: IbColors.errorRed,
-                          ),
                   ),
-                ),
-              );
-            },
-            itemCount: _controller.likeItems.length,
-          ),
-        );
-      }),
+                );
+              },
+              itemCount: _controller.likeItems.length,
+            ),
+          );
+        }),
+      ),
     );
   }
 }
