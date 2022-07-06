23ccdb732bec22418d931fef70d136d9eb713c59
Junjie Yang <yjj950406@gmail.com>
2022-07-06 14:42:28 -0400

    remove shake dep

--- lib/backend/controllers/user_controllers/icebreaker_controller.dart
+++ lib/backend/controllers/user_controllers/icebreaker_controller.dart
@@ -8,7 +8,6 @@ import 'package:icebr8k/backend/managers/Ib_analytics_manager.dart';
 import 'package:icebr8k/backend/models/icebreaker_models/ib_collection.dart';
 import 'package:icebr8k/backend/models/icebreaker_models/icebreaker.dart';
 import 'package:icebr8k/backend/services/user_services/icebreaker_db_service.dart';
-import 'package:shake/shake.dart';
 
 class IcebreakerController extends GetxController {
   IbCollection ibCollection;
@@ -22,7 +21,6 @@ class IcebreakerController extends GetxController {
   final ScrollController scrollController = ScrollController();
 
   late StreamSubscription icebreakerSub;
-  late ShakeDetector detector;
   CarouselController carouselController = CarouselController();
 
   IcebreakerController(this.ibCollection, {required this.isEdit});
@@ -36,14 +34,6 @@ class IcebreakerController extends GetxController {
 
   @override
   Future<void> onInit() async {
-    detector = ShakeDetector.waitForStart(
-        onPhoneShake: () async {
-          await shuffleCards();
-        },
-        shakeThresholdGravity: 2.5,
-        minimumShakeCount: 2);
-    detector.startListening();
-
     icebreakerSub = IcebreakerDbService()
         .listenToIcebreakerChange(ibCollection)
         .listen((event) {
@@ -75,30 +65,9 @@ class IcebreakerController extends GetxController {
     await HapticFeedback.selectionClick();
   }
 
-/*  void showShakePopup() {
-    if (isEdit) {
-      return;
-    }
-    Get.dialog(Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: IbDialog(
-        title: 'Shake your phone to shuffle questions',
-        subtitle: '',
-        content: Center(
-          child: SizedBox(
-              height: 200,
-              width: 200,
-              child: Lottie.asset('assets/images/shake.json')),
-        ),
-        showNegativeBtn: false,
-      ),
-    ));
-  }*/
-
   @override
   Future<void> onClose() async {
     await icebreakerSub.cancel();
-    detector.stopListening();
     super.onClose();
   }
 }
