ef622a6a2e93435ea1c756daaf1ff6c3c4ccac49
Junjie Yang <yjj950406@gmail.com>
2022-07-15 22:10:39 -0400

    fix handle dbstatus bug

--- lib/backend/controllers/user_controllers/auth_controller.dart
+++ lib/backend/controllers/user_controllers/auth_controller.dart
@@ -75,7 +75,7 @@ class AuthController extends GetxService {
     });
   }
 
-  Future<void> _handleDbStatus(
+  Future<bool> _handleDbStatus(
       DocumentSnapshot<Map<String, dynamic>> snapshot) async {
     final isRunning = snapshot.data()!['isRunning'] as bool;
     final note = snapshot.data()!['note'] as String;
@@ -95,7 +95,7 @@ class AuthController extends GetxService {
             showNegativeBtn: false,
           ),
           barrierDismissible: false);
-      return;
+      return false;
     }
 
     if (!isRunning) {
@@ -107,10 +107,11 @@ class AuthController extends GetxService {
           msg: note,
           backgroundColor: IbColors.primaryColor,
           isPersistent: true);
-      return;
+      return false;
     } else {
       ibUtils.closeAllSnackbars();
     }
+    return true;
   }
 
   @override
@@ -259,7 +260,7 @@ class AuthController extends GetxService {
   Future<void> navigateToCorrectPage() async {
     try {
       final statusSnap = await IbDbStatusService().queryStatus();
-      await _handleDbStatus(statusSnap);
+      final isOkay = await _handleDbStatus(statusSnap);
 
       final result = await Connectivity().checkConnectivity();
       if (result == ConnectivityResult.none) {
@@ -272,6 +273,11 @@ class AuthController extends GetxService {
       if (firebaseUser != null && firebaseUser!.emailVerified) {
         final IbUser? ibUser =
             await IbUserDbService().queryIbUser(firebaseUser!.uid);
+        if (!isOkay &&
+            ibUser != null &&
+            !ibUser.roles.contains(IbUser.kAdminRole)) {
+          return;
+        }
 
         /// check roles of the user
         if (ibUser != null &&
