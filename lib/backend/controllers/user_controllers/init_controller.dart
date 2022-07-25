d9061e8600518a2f0241009f74d777a2f9b43a19
Junjie <yjj950406@gmail.com>
2022-07-25 19:45:24 -0400

    added new product IDs for Ib Premium

--- lib/backend/controllers/user_controllers/init_controller.dart
+++ lib/backend/controllers/user_controllers/init_controller.dart
@@ -1,42 +1,21 @@
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:icebr8k/frontend/ib_colors.dart';
-import 'package:icebr8k/frontend/ib_utils.dart';
 
-class InitController extends GetxController {
+class InitController extends GetxService {
   final isLoading = true.obs;
   final hasError = false.obs;
-  late StreamSubscription networkSub;
 
   @override
   Future<void> onInit() async {
     super.onInit();
-    await GetStorage.init();
-    isLoading.value = false;
-  }
-
-  @override
-  Future<void> onReady() async {
-    networkSub = Connectivity()
-        .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      if (result == ConnectivityResult.none) {
-        IbUtils().showSimpleSnackBar(
-            msg: 'No Internet Connection',
-            backgroundColor: IbColors.errorRed,
-            isPersistent: true);
-      } else if (result != ConnectivityResult.bluetooth) {
-        Get.closeAllSnackbars();
-      }
-    });
-    super.onReady();
-  }
-
-  @override
-  Future<void> onClose() async {
-    await networkSub.cancel();
+    try {
+      await GetStorage.init();
+    } catch (e) {
+      hasError.value = true;
+    } finally {
+      isLoading.value = false;
+    }
   }
 }
